2ddd5c17e5114c85110348ee6b6f8f93656e99ad
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-12-26 19:13:37 -0300

    Also added comments. Commented out current workspace. Parsed the timestamp and the final result. Both scripts are delivering the same output.

--- baraction.R
+++ baraction.R
@@ -5,12 +5,16 @@ total_memory <- as.numeric(system2('cat',
     stdout = TRUE)[1])
 
 repeat {
-    current_desktop <- system2('xdotool', args = 'get_desktop', stdout = TRUE)
+    # current_desktop <- system2('xdotool', args = 'get_desktop', stdout = TRUE)
+    # Memory usage:
     memory_percent <- (total_memory - as.numeric(system2('cat',
         args = "/proc/meminfo | tr -d ' kB' | cut -d ':' -f 2",
         stdout = TRUE)[3])) / total_memory * 100
     show_memory <- paste0(c("RAM: ", round(memory_percent), "%"), collapse = "")
-    result <- c(show_memory, current_desktop, date())
-    cat(result, fill = TRUE)
+    # Current timestamp:
+    current_time <- format(Sys.time(), format = "%A, %d/%m/%Y %R")
+    # Parse and print result:
+    result <- c(show_memory, current_time)
+    cat(paste0(result, collapse = " | "), fill = TRUE)
     Sys.sleep(0.1)
 }
