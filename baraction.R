a80b3c9e43f8d51dd1023110aee1bb408066a8db
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-12-26 15:08:43 -0300

    Current workspace number added to `baraction.R`.

--- baraction.R
+++ baraction.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 
 repeat {
-  cat(c(date()), fill = TRUE)
+  Sys.setenv(workspace = system2('xdotool',
+    args = 'get_desktop', stdout = TRUE))
+  result <- c(Sys.getenv('workspace'), date())
+  cat(result, fill = TRUE)
   Sys.sleep(1)
 }
