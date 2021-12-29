7aa87df540f1a45dc0d4c0e0ea492b5a6a64891c
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-12-29 19:54:13 -0300

    Add volume level to baraction.R.

--- baraction.R
+++ baraction.R
@@ -6,6 +6,11 @@ total_memory <- as.numeric(system2("cat",
 
 repeat {
     # current_desktop <- system2('xdotool', args = 'get_desktop', stdout = TRUE)
+    # Volume:
+    volume <- paste0(c('Vol',
+        system2('pulsemixer', args = '--get-volume', stdout = TRUE)),
+        collapse = ': '
+    )
     # Memory usage:
     memory_percent <- (total_memory - as.numeric(system2("cat",
         args = "/proc/meminfo | tr -d ' kB' | cut -d ':' -f 2",
@@ -14,7 +19,7 @@ repeat {
     # Current timestamp:
     current_time <- format(Sys.time(), format = "%A, %d/%m/%Y %R")
     # Parse and print result:
-    result <- c(show_memory, current_time)
+    result <- c(volume, show_memory, current_time)
     cat(paste0(result, collapse = " | "), fill = TRUE)
     Sys.sleep(0.1)
 }
